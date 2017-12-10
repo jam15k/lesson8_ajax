c0576b01ed7eba1fc31111db0804f78395f6974f
Jorge Morejon <jam15k@my.fsu.edu>
2017-12-10 02:00:40 +0000

    Lesson 8 final commit

--- Movie.js
+++ Movie.js
@@ -32,7 +32,7 @@ $(document).ready(function(){
 
             $.ajax({
                 //Insert YOUR API Key after the '=' inside the single quotes.
-                url:'https://api.themoviedb.org/3/search/movie?api_key=INSERT-HERE',
+                url:'https://api.themoviedb.org/3/search/movie?api_key=c5d9a4c113602a92eab796f837e62c7f',
                 data: query
             })
             
@@ -56,10 +56,10 @@ $(document).ready(function(){
                  3.  Use the example above where the Movie Title was retrieved to complete this step!
                  
                  *********************************Insert Code Below********************************************/                
+            
+                $("#overview").html(json.results[0].overview);
                 
-                
-                
-
+            
                 /***********************Retrieve Movie Release Date from Server & Add to HTML Document***********
                  TASK #7:  
                  1.  Include the jQuery code below that will retrieve the movie release date from JSON object
@@ -71,8 +71,8 @@ $(document).ready(function(){
                  3.  Use the example above where the Movie Title was retrieved to complete this step!
                  
                  *********************************Insert Code Below********************************************/               
-
-
+                
+                $("#release_date").html(json.results[0].release_date);
 
 
                  /***********************Retrieve Movie Popularity Rate from Server & Add to HTML Document******
@@ -86,8 +86,8 @@ $(document).ready(function(){
                  3.  Use the example above where the Movie Title was retrieved to complete this step!
                  
                  *********************************Insert Code Below********************************************/   
-
-  
+                
+                $("#popularity").html(json.results[0].popularity);
 
                 
                  /*****************Retrieve Movie Original Language from Server & Add to HTML Document***********
@@ -102,7 +102,9 @@ $(document).ready(function(){
                  
                  *********************************Insert Code Below********************************************/                 
 
-
+                $("#original_language").html(json.results[0].original_language);
+                
+                
 
                 //The following code retrieves the movie poster path from the JSON object retrieved from the TMDB 
                 //Server and places the poster image on the HTML page, where the id="poster_path".  It took TWO
--- README.md
+++ README.md
@@ -4,19 +4,18 @@
 LIS 2360:  Web Application Development
 
 ### Assignment Date:  
-(Month Day, Year)
+12/9/17
 
 ### Student Name:  
-(First and Last Name)
+Jorge Morejon
 
 ### Project Description:
-(In your own words, briefly describe the assignment.)
+Make a website that retrieves search information
 
 ### View Project:
-(Replace this statement with your Github Page URL that was created when you 
- published the project.)
+https://jam15k.github.io/lesson8_ajax/
 
 ### Lessons Learned in the Assignment:
-1. (Briefly describe a lesson/concept learned in this lesson.)
-2. (Briefly describe a lesson/concept learned in this lesson.)
-3. (Briefly describe a lesson/concept learned in this lesson.)
+1. What movie database API is and how to use it
+2. How to use the google chrome javascript console to look up for different things
+3. How to retrieve and dispaly text
